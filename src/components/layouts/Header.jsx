758662afd14d08807ec152f86b4ad52fa8d9fab9
abdunnoorfaheem <abdunnoor3030@gmail.com>
2026-05-05 22:35:10 +0600

    header section added

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -1,9 +1,32 @@
-
+import Logo from "/src/assets/logo.png"
+import Container from "../Container";
+import Image from "../Image"
 
 const Header = () => {
   return (
     <>
-      <h1>Header</h1>
+      <div className="">
+        <Container>
+          <div className="flex justify-between items-center py-2">
+          <div className="">
+           <Image imgSrc={Logo}/>
+          </div>
+          <div className="">
+            <ul className="flex gap-x-5 text-[#616161] font-poppins">
+              <li>Home</li>
+              <li>Tours</li>
+              <li>Booking</li>
+              <li>Pages</li>
+              <li>Features</li>
+              <li>Blog</li>
+            </ul>
+          </div>
+          <div className="">
+            <button className="bg-[#007F5F] rounded-[17px] px-9 py-4 text-white font-semibold font-poppins">Book Now</button>
+          </div>
+        </div>
+        </Container>
+      </div>
     </>
   )
 }
